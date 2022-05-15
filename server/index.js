3fcc06f6d9094b011d8fa26b45bbf9e4796ff0d0
Rizky Agung <agungrizky1805@gmail.com>
2022-05-15 19:38:32 +0700

    setup mern

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 const wishlistRoutes = require('./routes/wishlist')
 
 require('dotenv').config();
@@ -12,11 +13,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
 const PORT = process.env.PORT || 80;
 
 mongoose.connect(process.env.MONGO_URI);
 
 app.use('/wishlist', wishlistRoutes);
+
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT)
