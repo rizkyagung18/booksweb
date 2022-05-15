01954072420fca7e846caf942c0a96f6c8842801
Rizky Agung <agungrizky1805@gmail.com>
2022-05-15 20:47:14 +0700

    heroku

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI);
 
