c0bc5ca436833b3aba68172db7d33499040027ed
Rizky Agung <agungrizky1805@gmail.com>
2022-05-15 20:51:35 +0700

    heroku

--- server/index.js
+++ server/index.js
@@ -13,16 +13,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI);
 
 app.use('/wishlist', wishlistRoutes);
 
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
